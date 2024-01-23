6159be48b6116cb1743e192c4fa7e4aea8648a47
Flo Holst <florianholst97@gmail.com>
2024-01-23 12:37:24 +0100

    actual lsp and stuff

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,28 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- here you can setup the language servers 
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {"gopls"},
+  handlers = {
+    lsp_zero.default_setup,
+    require'lspconfig'.pylsp.setup{
+      settings = {
+        pylsp = {
+          plugins = {
+            pycodestyle = {
+              ignore = {'W391'},
+              maxLineLength = 100
+            }
+          }
+        }
+      }
+    }
+  },
+})
