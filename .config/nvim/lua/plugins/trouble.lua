c3ad52e01d41f11a6ae0e730ecb4aa582e85a19e
Florian Holst <florianholst97@gmail.com>
2026-07-02 22:29:08 +0200

    add nvim submodule ]

--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-        {"<leader>tt", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"},
-        {"<leader>pt", "<cmd>Trouble diagnostics toggle<cr>"},
-    },
-}
